914125342889e96ea3abba9cb861d71b6aaeb48c
jeromeVitali <jerome.vitali@le-campus-numerique.fr>
2019-02-24 19:13:50 +0100

    ajout categorie page boutique

--- boutique.php
+++ boutique.php
@@ -1,42 +1,24 @@
 <?php
 
-//BUG AFFICHAGE !!!!! VOIR EPISODE 6
 
 require_once('includes/header.php'); 
 
 require_once('includes/sidebar.php'); 
 
 
+$select = $db->query("SELECT * FROM category");
+while($s = $select->fetch(PDO::FETCH_OBJ)){
 
+    ?>
 
+    <a href="?category=<?php echo $s->name;?>"><h3><?php echo $s->name ?></h3></a>
 
-$select = $db->prepare("SELECT * FROM products");
-$select->execute();
+    <?php  
 
-while($s=$select->fetch(PDO::FETCH_OBJ)){
+}
 
-?>
-<img src="admin/imgs/<?php echo $s->title ?>.jpg"/>
-<h2><?php echo $s->title; ?></h2> <br> 
-<h5><?php echo $s->descript; ?></h5> <br> 
-<h4><?php echo $s->price; ?> €</h4> <br> 
-
- <br> 
- <br> 
-
-
-
-
-<?php
- }
-
-
- require_once('includes/footer.php'); 
+require_once('includes/footer.php'); 
 
 
 
 ?>
-
-
-
-
