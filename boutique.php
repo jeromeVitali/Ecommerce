581ce8a86029eb7b76afc5ac459d293a69b7f470
jeromeVitali <jerome.vitali@le-campus-numerique.fr>
2019-02-25 17:34:16 +0100

    details des objs

--- boutique.php
+++ boutique.php
@@ -1,50 +1,71 @@
 <?php
 
+require_once 'includes/header.php';
 
-require_once('includes/header.php'); 
+require_once 'includes/sidebar.php';
 
-require_once('includes/sidebar.php'); 
+if (isset($_GET['show'])){
 
-if(isset($_GET['category'])){
+    $product = $_GET['show'];
 
-    $category=$_GET['category'];
-    $select = $db->prepare("SELECT * FROM products WHERE category='$category' ");
+    $select = $db->prepare("SELECT * FROM products WHERE title='$product' ");
     $select->execute();
 
-    while($s=$select->fetch(PDO::FETCH_OBJ)){
+    $s = $select->fetch(PDO::FETCH_OBJ);
 
-?>
-
+    ?>
+<br>
+<br>
+<div style="text-align:center;">
 <img src="admin/imgs/<?php echo $s->title; ?>.jpg"/>
-<h2><?php echo $s->title; ?> </h2>
+<h1> <?php echo $s->title; ?> </h1>
+<h5><?php echo $s->descript; ?></h5>
+<h4><?php echo $s->price; ?> €</h4>
+</div>
+
+
+<?php
+
+} else {
+
+    if (isset($_GET['category'])) {
+
+        $category = $_GET['category'];
+        $select = $db->prepare("SELECT * FROM products WHERE category='$category' ");
+        $select->execute();
+
+        while ($s=$select->fetch(PDO::FETCH_OBJ)) {
+
+
+            ?>
+<a href="?show=<?php echo $s->title; ?>"> <img src="admin/imgs/<?php echo $s->title; ?>.jpg"/></a>
+<a href="?show=<?php echo $s->title; ?>"> <h2><?php echo $s->title; ?> </h2> </a>
 <h5><?php echo $s->descript; ?> </h5>
 <h4><?php echo $s->price; ?> €</h4>
 <br><br>
 
 <?php
 
+        }
 
-}
+    } else {
 
+        $select = $db->query("SELECT * FROM category");
 
-}else{
+        while ($s = $select->fetch(PDO::FETCH_OBJ)) {
 
-    $select = $db->query("SELECT * FROM category");
-
-    while($s=$select->fetch(PDO::FETCH_OBJ)){
-
-?>
+            ?>
 
 <a href="?category=<?php echo $s->name; ?>">  <h3> <?php echo $s->name; ?> </h3> </a>
 
 <?php
 
+        }
+
+    }
 
 }
 
-}
-
-require_once('includes/footer.php'); 
-
+require_once 'includes/footer.php';
 
 ?>
--- includes/header.php
+++ includes/header.php
@@ -30,12 +30,7 @@
 			<li><a href="index.php">Accueil</a></li>
 			<li><a href="boutique.php">Boutique</a></li>
 			<li><a href="panier.php">Panier</a></li>
-			<?php if(!isset($_SESSION['user_id'])){?>
-			<li><a href="register.php">S'inscrire</a></li>
-			<li><a href="connect.php">Se connecter</a></li>
-			<?php }else{ ?>
-			<li><a href="my_account.php">Mon compte</a></li>
-			<?php } ?>
 			<li><a href="conditions_generales_de_vente.php">Conditions Generales de Vente</a></li>
 		</ul>
 	</header>
+</html>
